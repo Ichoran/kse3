c125417eb5f52ad0f4d7e6156dcd8ecd12b4d4e5
Rex Kerr <ichoran@gmail.com>
2023-02-09 18:34:18 -0800

    Split EioConversion into EioBase64 and EioBase85

--- eio/src/Conversion.scala
+++ eio/src/Conversion.scala
@@ -13,7 +13,7 @@ import kse.flow.{given, _}
 import kse.maths.{given, _}
 import kse.maths.packed.{given, _}
 
-object EioConversion {
+object EioBase64 {
   private val encode64urlTable: Array[Byte] = Array(
     'A'.toByte, 'B'.toByte, 'C'.toByte, 'D'.toByte, 'E'.toByte, 'F'.toByte, 'G'.toByte, 'H'.toByte,
     'I'.toByte, 'J'.toByte, 'K'.toByte, 'L'.toByte, 'M'.toByte, 'N'.toByte, 'O'.toByte, 'P'.toByte,
@@ -42,7 +42,7 @@ object EioConversion {
     46, 47, 48, 49, 50, 51               //117-122
   )
 
-  def encodeUrlStyleBase64Range(raw: Array[Byte], i0: Int, iN: Int, lineLength: Int = Int.MaxValue): Array[Byte] =
+  def encodeUrlRange(raw: Array[Byte], i0: Int, iN: Int, lineLength: Int = Int.MaxValue): Array[Byte] =
     if i0 < 0 || iN > raw.length then throw new RuntimeException(s"Invalid input range: $i0 until $iN in input of length ${raw.length}")
     var l = ((iN - i0)*4 + 2)/3
     val n = if lineLength < 4 then 1 else lineLength/4
@@ -80,13 +80,13 @@ object EioConversion {
         a(j+2) = encode64urlTable( chunk        & 0x2F)
     a
 
-  def encodeUrlStyleBase64(raw: Array[Byte], lineLength: Int = Int.MaxValue): Array[Byte] =
-    encodeUrlStyleBase64Range(raw, 0, raw.length, lineLength)
+  def encodeUrl(raw: Array[Byte], lineLength: Int = Int.MaxValue): Array[Byte] =
+    encodeUrlRange(raw, 0, raw.length, lineLength)
 
-  def stringEncodeUrlStyleBase64(raw: Array[Byte], lineLength: Int = Int.MaxValue): String =
-    new String(encodeUrlStyleBase64Range(raw, 0, raw.length, lineLength), ISO_8859_1)
+  def stringEncodeUrl(raw: Array[Byte], lineLength: Int = Int.MaxValue): String =
+    new String(encodeUrlRange(raw, 0, raw.length, lineLength), ISO_8859_1)
 
-  def decodeAnyBase64RangeInto(encoded: Array[Byte], i0: Int, iN: Int)(target: Array[Byte], index: Int): Int Or Err = Or.Ret:
+  def decodeRangeInto(encoded: Array[Byte], i0: Int, iN: Int)(target: Array[Byte], index: Int): Int Or Err = Or.Ret:
     var bits = 0
     var nb = 0
     var i = i0
@@ -124,20 +124,20 @@ object EioConversion {
     if bits != 0 then Err.break(s"Trailing bits: ${if bits.byte(1) != 0 then bits.byte(1).bitString else bits.byte(0).bitString}; Base64 data must be incomplete")
     j - index
 
-  def decodeAnyBase64Into(encoded: Array[Byte])(target: Array[Byte], index: Int): Int Or Err =
-    decodeAnyBase64RangeInto(encoded, 0, encoded.length)(target, index)
+  def decodeInto(encoded: Array[Byte])(target: Array[Byte], index: Int): Int Or Err =
+    decodeRangeInto(encoded, 0, encoded.length)(target, index)
 
-  def decodeAnyBase64Range(encoded: Array[Byte], i0: Int, iN: Int): Array[Byte] Or Err =
+  def decodeRange(encoded: Array[Byte], i0: Int, iN: Int): Array[Byte] Or Err =
     if i0 < 0 || iN > encoded.length then Err.or(s"Base64 decode range $i0 until $iN not within array of length ${encoded.length}")
     else
       val a = new Array[Byte]((((iN - i0).toLong*3 + 2) / 4).toInt)
-      decodeAnyBase64RangeInto(encoded, i0, iN)(a, 0).map(i => a.shrinkCopy(i))
+      decodeRangeInto(encoded, i0, iN)(a, 0).map(i => a.shrinkCopy(i))
 
-  def decodeAnyBase64(encoded: Array[Byte]): Array[Byte] Or Err =
+  def decode(encoded: Array[Byte]): Array[Byte] Or Err =
     val a = new Array[Byte](((encoded.length.toLong*3 + 2)/4).toInt)
-    decodeAnyBase64RangeInto(encoded, 0, encoded.length)(a, 0).map(i => a.shrinkCopy(i))
+    decodeRangeInto(encoded, 0, encoded.length)(a, 0).map(i => a.shrinkCopy(i))
 
-  def decodeAnyBase64RangeInto(encoded: String, i0: Int, iN: Int)(target: Array[Byte], index: Int): Int Or Err = Or.Ret:
+  def decodeRangeInto(encoded: String, i0: Int, iN: Int)(target: Array[Byte], index: Int): Int Or Err = Or.Ret:
     var bits = 0
     var nb = 0
     var i = i0
@@ -175,19 +175,22 @@ object EioConversion {
     if bits != 0 then Err.break(s"Trailing bits: ${if bits.byte(1) != 0 then bits.byte(1).bitString else bits.byte(0).bitString}; Base64 data must be incomplete")
     j - index
 
-  def decodeAnyBase64Into(encoded: String)(target: Array[Byte], index: Int): Int Or Err =
-    decodeAnyBase64RangeInto(encoded, 0, encoded.length)(target, index)
+  def decodeInto(encoded: String)(target: Array[Byte], index: Int): Int Or Err =
+    decodeRangeInto(encoded, 0, encoded.length)(target, index)
 
-  def decodeAnyBase64Range(encoded: String, i0: Int, iN: Int): Array[Byte] Or Err =
+  def decodeRange(encoded: String, i0: Int, iN: Int): Array[Byte] Or Err =
     if i0 < 0 || iN > encoded.length then Err.or(s"Base64 decode range $i0 until $iN not within string of length ${encoded.length}")
     else
       val a = new Array[Byte]((((iN - i0).toLong*3) / 4).toInt)
-      decodeAnyBase64RangeInto(encoded, i0, iN)(a, 0).map(i => a.shrinkCopy(i))
+      decodeRangeInto(encoded, i0, iN)(a, 0).map(i => a.shrinkCopy(i))
 
-  def decodeAnyBase64(encoded: String): Array[Byte] Or Err =
+  def decode(encoded: String): Array[Byte] Or Err =
     val a = new Array[Byte](((encoded.length.toLong*3)/4).toInt)
-    decodeAnyBase64RangeInto(encoded, 0, encoded.length)(a, 0).map(i => a.shrinkCopy(i))
+    decodeRangeInto(encoded, 0, encoded.length)(a, 0).map(i => a.shrinkCopy(i))
+}
 
+
+object EioBase85 {
   private def encodeTable_to_decodeTable(encoder: Array[Byte]): Array[Byte] =
     if encoder.length == 0 then encoder
     else
@@ -209,9 +212,9 @@ object EioConversion {
       if decoder(23) == -1 then decoder(23) = (encoder.length).toByte
       decoder
 
-  private val encode85asciiTable: Array[Byte] = Array.tabulate(85)(i => (i+33).toByte)
+  private val encodeAsciiTable: Array[Byte] = Array.tabulate(85)(i => (i+33).toByte)
 
-  private val encodeZ85Table: Array[Byte] = Array(
+  private val encodeZmqTable: Array[Byte] = Array(
     '0'.toByte, '1'.toByte, '2'.toByte, '3'.toByte, '4'.toByte, '5'.toByte, '6'.toByte, '7'.toByte, '8'.toByte, '9'.toByte,
     'a'.toByte, 'b'.toByte, 'c'.toByte, 'd'.toByte, 'e'.toByte, 'f'.toByte, 'g'.toByte, 'h'.toByte, 'i'.toByte, 'j'.toByte,
     'k'.toByte, 'l'.toByte, 'm'.toByte, 'n'.toByte, 'o'.toByte, 'p'.toByte, 'q'.toByte, 'r'.toByte, 's'.toByte, 't'.toByte,
@@ -223,11 +226,11 @@ object EioConversion {
     '}'.toByte, '@'.toByte, '%'.toByte, '$'.toByte, '#'.toByte
   )
 
-  private val decodeZ85Table = encodeTable_to_decodeTable(encodeZ85Table)
+  private val decodeZmqTable = encodeTable_to_decodeTable(encodeZmqTable)
 
-  private val decode85asciiTable = encodeTable_to_decodeTable(encode85asciiTable)
+  private val decodeAsciiTable = encodeTable_to_decodeTable(encodeAsciiTable)
 
-  private def encode85RangeWithTable(raw: Array[Byte], i0: Int, iN: Int, table: Array[Byte]): Array[Byte] =
+  private def encodeRangeWithTable(raw: Array[Byte], i0: Int, iN: Int, table: Array[Byte]): Array[Byte] =
     if i0 < 0 || iN > raw.length then throw new RuntimeException(s"Range $i0 until $iN out of bounds of array of length ${raw.length}")
     val l = ((iN - i0)*5 + 3)/4
     if l > Int.MaxValue - 7 then throw new RuntimeException(s"Overly long Base85 encode: would require $l bytes")
@@ -273,7 +276,7 @@ object EioConversion {
           result(j+3) = table(c)
     result
 
-  def encode85ascii(raw: Array[Byte]): Array[Byte] =
+  def encodeAscii(raw: Array[Byte]): Array[Byte] =
     inline def table(i: Int) = (i + 33).toByte
     val l = (raw.length.toLong*5 + 3)/4
     if l > Int.MaxValue - 7 then throw new RuntimeException(s"Overly long Base85 encode: would require $l bytes")
@@ -319,19 +322,19 @@ object EioConversion {
           result(j+3) = table(c)
     result
 
-  def stringEncode85ascii(raw: Array[Byte]): String =
-    new String(encode85ascii(raw), ISO_8859_1)
+  def stringEncodeAscii(raw: Array[Byte]): String =
+    new String(encodeAscii(raw), ISO_8859_1)
 
-  def encode85asciiRange(raw: Array[Byte], i0: Int, iN: Int): Array[Byte] = encode85RangeWithTable(raw, i0, iN, encode85asciiTable)
+  def encodeAsciiRange(raw: Array[Byte], i0: Int, iN: Int): Array[Byte] = encodeRangeWithTable(raw, i0, iN, encodeAsciiTable)
 
-  def encodeZ85(raw: Array[Byte]): Array[Byte] = encode85RangeWithTable(raw, 0, raw.length, encodeZ85Table)
+  def encodeZmq(raw: Array[Byte]): Array[Byte] = encodeRangeWithTable(raw, 0, raw.length, encodeZmqTable)
 
-  def stringEncodeZ85(raw: Array[Byte]): String =
-    new String(encodeZ85(raw), ISO_8859_1)
+  def stringEncodeZmq(raw: Array[Byte]): String =
+    new String(encodeZmq(raw), ISO_8859_1)
 
-  def encodeZ85Range(raw: Array[Byte], i0: Int, iN: Int): Array[Byte] = encode85RangeWithTable(raw, i0, iN, encodeZ85Table)
+  def encodeZmqRange(raw: Array[Byte], i0: Int, iN: Int): Array[Byte] = encodeRangeWithTable(raw, i0, iN, encodeZmqTable)
 
-  private def decode85WithTable(encoded: Array[Byte], table: Array[Byte]): Array[Byte] Or Err = Or.Ret:
+  private def decodeWithTable(encoded: Array[Byte], table: Array[Byte]): Array[Byte] Or Err = Or.Ret:
     val l = (encoded.length.toLong*4)/5
     var a = new Array[Byte](l.toInt)
     var lead: Int = 0
@@ -372,17 +375,20 @@ object EioConversion {
       if lead > 82 || (lead == 82 && rest > 14516045) then Err.break(s"Over-full Base85 number $lead*85^4 + $rest at end of input")
       rest = lead*52200625 + rest
       a(j) = (rest >>> 24).toByte
+      j += 1
       if n > 2 then
-        a(j+1) = ((rest >>> 16) & 0xFF).toByte
+        a(j) = ((rest >>> 16) & 0xFF).toByte
+        j += 1
         if n > 3 then
-          a(j+2) = ((rest >>> 8) & 0xFF).toByte
-    a
+          a(j) = ((rest >>> 8) & 0xFF).toByte
+          j += 1
+    a.shrinkCopy(j)
 
-  def decodeZ85(encoded: Array[Byte]): Array[Byte] Or Err = decode85WithTable(encoded, decodeZ85Table)
+  def decodeZmq(encoded: Array[Byte]): Array[Byte] Or Err = decodeWithTable(encoded, decodeZmqTable)
 
-  def decode85ascii(encoded: Array[Byte]): Array[Byte] Or Err = decode85WithTable(encoded, decode85asciiTable)
+  def decodeAscii(encoded: Array[Byte]): Array[Byte] Or Err = decodeWithTable(encoded, decodeAsciiTable)
 
-  private def decode85WithTable(encoded: String, table: Array[Byte]): Array[Byte] Or Err = Or.Ret:
+  private def decodeWithTable(encoded: String, table: Array[Byte]): Array[Byte] Or Err = Or.Ret:
     val l = (encoded.length.toLong*4)/5
     var a = new Array[Byte](l.toInt)
     var lead: Int = 0
@@ -423,15 +429,18 @@ object EioConversion {
       if lead > 82 || (lead == 82 && rest > 14516045) then Err.break(s"Over-full Base85 number $lead*85^4 + $rest at end of input")
       rest = lead*52200625 + rest
       a(j) = (rest >>> 24).toByte
+      j += 1
       if n > 2 then
-        a(j+1) = ((rest >>> 16) & 0xFF).toByte
+        a(j) = ((rest >>> 16) & 0xFF).toByte
+        j += 1
         if n > 3 then
-          a(j+2) = ((rest >>> 8) & 0xFF).toByte
-    a
+          a(j) = ((rest >>> 8) & 0xFF).toByte
+          j += 1
+    a.shrinkCopy(j)
 
-  def decodeZ85(encoded: String): Array[Byte] Or Err = decode85WithTable(encoded, decodeZ85Table)
+  def decodeZmq(encoded: String): Array[Byte] Or Err = decodeWithTable(encoded, decodeZmqTable)
 
-  def decode85ascii(encoded: String): Array[Byte] Or Err = decode85WithTable(encoded, decode85asciiTable)
+  def decodeAscii(encoded: String): Array[Byte] Or Err = decodeWithTable(encoded, decodeAsciiTable)
 
 
   /*
@@ -455,43 +464,43 @@ extension (underlying: Array[Byte]) {
   inline def stringEncode64basic = Base64.getEncoder.encodeToString(underlying)
   inline def stringEncode64url = Base64.getUrlEncoder.encodeToString(underlying)
   inline def stringEncode64mime = Base64.getMimeEncoder.encodeToString(underlying)
-  inline def stringEncode64lines = EioConversion.stringEncodeUrlStyleBase64(underlying, 76)
+  inline def stringEncode64lines = EioBase64.stringEncodeUrl(underlying, 76)
 
-  inline def stringEncode85 = EioConversion.stringEncodeZ85(underlying)
-  inline def stringEncodeZ85 = EioConversion.stringEncodeZ85(underlying)
-  inline def stringEncode85ascii = EioConversion.stringEncode85ascii(underlying)
+  inline def stringEncode85 = EioBase85.stringEncodeZmq(underlying)
+  inline def stringEncode85zmq = EioBase85.stringEncodeZmq(underlying)
+  inline def stringEncode85ascii = EioBase85.stringEncodeAscii(underlying)
 
   inline def encode64 = Base64.getUrlEncoder.encode(underlying)
   inline def encode64basic = Base64.getEncoder().encode(underlying)
   inline def encode64url = Base64.getUrlEncoder.encode(underlying)
   inline def encode64mime = Base64.getMimeEncoder().encode(underlying)
-  inline def encode64lines = EioConversion.encodeUrlStyleBase64(underlying, 76)
+  inline def encode64lines = EioBase64.encodeUrl(underlying, 76)
 
-  inline def decode64 = EioConversion.decodeAnyBase64(underlying)
+  inline def decode64 = EioBase64.decode(underlying)
   inline def decode64basic = safe{ Base64.getDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
   inline def decode64url = safe{ Base64.getUrlDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
   inline def decode64mime = safe{ Base64.getMimeDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
 
-  inline def encode85 = EioConversion.encodeZ85(underlying)
-  inline def encodeZ85 = EioConversion.encodeZ85(underlying)
-  inline def encode85ascii = EioConversion.encode85ascii(underlying)
+  inline def encode85 = EioBase85.encodeZmq(underlying)
+  inline def encode85zmq = EioBase85.encodeZmq(underlying)
+  inline def encode85ascii = EioBase85.encodeAscii(underlying)
 
-  inline def decode85 = EioConversion.decodeZ85(underlying)
-  inline def decodeZ85 = EioConversion.decodeZ85(underlying)
-  inline def decode85ascii = EioConversion.decode85ascii(underlying)
+  inline def decode85 = EioBase85.decodeZmq(underlying)
+  inline def decode85zmq = EioBase85.decodeZmq(underlying)
+  inline def decode85ascii = EioBase85.decodeAscii(underlying)
 }
 
 extension (underlying: String) {
   inline def bytes: Array[Byte] = underlying.getBytes(UTF_8)
 
-  inline def decode64 = EioConversion.decodeAnyBase64(underlying)
+  inline def decode64 = EioBase64.decode(underlying)
   inline def decode64basic = safe{ Base64.getDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
   inline def decode64url = safe{ Base64.getUrlDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
   inline def decode64mime = safe{ Base64.getMimeDecoder().decode(underlying) }.mapAlt(t => Err(t.getMessage))
 
-  inline def decode85 = EioConversion.decodeZ85(underlying)
-  inline def decodeZ85 = EioConversion.decodeZ85(underlying)
-  inline def decode85ascii = EioConversion.decode85ascii(underlying)
+  inline def decode85 = EioBase85.decodeZmq(underlying)
+  inline def decode85zmq = EioBase85.decodeZmq(underlying)
+  inline def decode85ascii = EioBase85.decodeAscii(underlying)
 }
 
 extension (underlying: java.util.zip.ZipEntry) {
